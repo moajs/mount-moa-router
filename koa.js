ed98c49cdd2cc8a4c9ba3a98041b709466e56434
i5ting <i5ting@126.com>
2017-11-13 21:38:28 +0800

    lazycommmit, please ignore

---
+++ koa.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const Koa = require('koa')
+const app = new Koa()
+
+const router = require('moa-router')()
+// router.type = 'koa'
+
+router.get('/', (ctx, next) => {
+  ctx.body = {'path': 'root'}
+})
+
+router.on('GET', '/test', (ctx, next) => {
+  ctx.body = {'hello': 'world'}
+})
+
+app.use(router.routes())
+
+app.use(async function (ctx, next) {
+  ctx.body = "default"
+})
+
+const server = http.createServer(app.callback())
+
+server.listen(3000, err => {
+  if (err) throw err
+  console.log('Server listening on: http://localhost:3000')
+})
