4ce057164c807c062189a177bbe1f990830648a5
i5ting <i5ting@126.com>
2017-11-16 02:30:29 +0800

    lazycommmit, please ignore

--- test5.js
+++ test5.js
@@ -1,54 +1,77 @@
-var routeruser = require('moa-router')()
+// var routeruser = require('moa-router')()
 
 
-routeruser.tree.prefix = '/users/'
+// routeruser.tree.prefix = '/users/'
 
 
-const router = require('./routes/api/users')
+// const router = require('./routes/api/users')
 
-require('log-n')(router)
+// require('log-n')(router)
 
-wrap(routeruser, '/api/', router)
+// wrap(routeruser, '/api/', router)
 
-function wrap(parent, prefix, router) {
-  var empty = require('moa-router')()
-  empty.tree.prefix = prefix
+// function wrap(parent, prefix, router) {
+//   var empty = require('moa-router')()
+//   empty.tree.prefix = prefix
 
-  router.tree.prefix = prefix
+//   router.tree.prefix = prefix
 
-  var arr = parent.tree.children
+//   var arr = parent.tree.children
 
-  arr = arr.concat(router.tree)
-  console.log(router)
+//   arr = arr.concat(router.tree)
+//   console.log(router)
 
-  empty.tree.children = arr //.concat(router.tree.children)
-  empty.tree.numberOfChildren = arr.length
+//   empty.tree.children = arr //.concat(router.tree.children)
+//   empty.tree.numberOfChildren = arr.length
 
-  require('log-n')(empty);
+//   require('log-n')(empty);
 
-  var all = empty.tree.children
-  console.log(all)
-
-
-  console.log(parent.tree.children)
+//   var all = empty.tree.children
+//   console.log(all)
 
 
-  parent.tree.children = all.concat(parent.tree.children)
-  parent.tree.numberOfChildren = all.length
+//   console.log(parent.tree.children)
 
 
-  require('log-n')(parent);
+//   parent.tree.children = all.concat(parent.tree.children)
+//   parent.tree.numberOfChildren = all.length
 
+
+//   require('log-n')(parent);
+
+// }
+
+// const http = require('http')
+// const Koa = require('koa')
+// const app = new Koa()
+
+
+// // require('log-n')(routeruser);
+
+
+// app.use(routeruser.routes())
+
+// app.listen(3000)
+
+
+
+
+
+var a = {
+  on: function (aa) {
+    console.log(aa)
+  },
+
+  f: function () {
+    console.log(f)
+    original.apply(this, arguments);
+    return this.on.apply(this, arguments);
+  }
 }
 
-const http = require('http')
-const Koa = require('koa')
-const app = new Koa()
+
+var origin = a.on
 
 
-// require('log-n')(routeruser);
 
-
-app.use(routeruser.routes())
-
-app.listen(3000)
+a.on('23332')
